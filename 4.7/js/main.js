6976752206192e9315edfee2831c1122fee8e72f
MJurayev <jurayevmansurbek667@gmail.com>
2022-06-10 20:39:59 +0500

    initial

---
+++ 4.7/js/main.js
@@ -0,0 +1,30 @@
+// var currentElement = document.querySelector('.current-element')
+
+// console.log(document.body.parentElement)
+
+var fifthDivElement = document.querySelector(".fifth")
+
+// console.log(fifthDivElement.closest("div"))
+
+// var fourthEl = document.querySelector(".fourth")
+// console.log(fourthEl.children)
+
+
+var list = document.querySelector(".list")
+
+console.log(list)
+let i=0;
+console.log(list.children.length)
+
+
+// const timer = setInterval(() => {
+//     list.children[i].style.backgroundColor = "red";
+//     if(i===list.children.length)clearInterval(timer)
+//     i++
+// }, 1000)
+
+const todo = {
+    // data:"somtenithi",
+    // delete()
+    // deleteTodo()
+}
